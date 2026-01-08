d34c20d715e98f7e1f9903977c4fd0361012a417
Mihika jain <mihika2312@gmail.com>
2026-01-08 13:02:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, Form
+from model import generate_questions
+from resume_parser import extract_resume_text
 
-app = FastAPI()
+app = FastAPI(title="TalentScout Hiring Assistant")
 
 @app.get("/")
 def root():
-    return {"status": "ok"}
+    return {"status": "TalentScout API running"}
+
+@app.post("/analyze")
+async def analyze_candidate(
+    resume: UploadFile = File(None),
+    manual_text: str = Form(None)
+):
+    if not resume and not manual_text:
+        return {"error": "Provide resume or manual input"}
+
+    if resume:
+        profile_text = extract_resume_text(resume.file, resume.filename)
+    else:
+        profile_text = manual_text
+
+    questions = generate_questions(profile_text)
+
+    return {
+        "status": "success",
+        "questions": questions
+    }
